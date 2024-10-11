ad3cda963d9ed7e03ecf3b8102145a4496565813
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-11 18:47:50 +0500

    add to commit

--- pratice_next_js/src/app/about/page.js
+++ pratice_next_js/src/app/about/page.js
@@ -1,6 +1,6 @@
 function About() {
     return (
-        <h1 className="text-3xl text-gray-500 font-bold">
+        <h1 className="text-3xl text-gray-500 font-bold underline text-center mt-3">
             About Page
         </h1>
     )
