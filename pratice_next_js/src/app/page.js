fa7759374f400f9661c5cb5595cb3236105e01e1
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-11 18:29:54 +0500

    add to changes

--- pratice_next_js/src/app/page.js
+++ pratice_next_js/src/app/page.js
@@ -5,11 +5,21 @@ export default function Home() {
   return (
     <header className="bg-red-400 p-4">
       <nav>
-        <ul>
+        <ul className="flex justify-center space-x-6">
         <li>
-          <Link href="#" >Home</Link>
+        <a href="#home" className="text-white hover:bg-gray-600 px-3 py-2 rounded">
+                Home
+              </a>
         </li>
+        <li>
+          <Link href="/about" className="text-white hover:bg-orange-400 px-3 py-2 rounded">About </Link>
+        </li>
+        <li>
+          <Link href="/contact" className="text-white hover:bg-gray-400 px-3 py-2 rounded">Contact </Link>
+        </li>
+
         </ul>
       </nav>
     </header>
+  )
 }
