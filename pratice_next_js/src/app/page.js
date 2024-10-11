0e16d31662c2cc673397a507d2e6df405325eac7
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-11 18:14:30 +0500

    add to commit

--- pratice_next_js/src/app/page.js
+++ pratice_next_js/src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <header>
+    <header className="bg-red-400 p-4">
       <nav>
         <ul>
-          
+        <li>
+          <Link href="#">Home</Link>
+        </li>
         </ul>
       </nav>
     </header>
