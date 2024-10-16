2678fdd845723ebfdcdc7666241973460e0455c6
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-16 20:01:06 +0500

    create blog page

---
+++ pratice_next_js/src/app/blog/page.js
@@ -0,0 +1,8 @@
+function Blog() {
+    return (
+        <h1 className="text-3xl text-gray-500 font-bold underline text-center mt-3">
+            My Blog
+        </h1>
+    )
+}
+export default Blog;
