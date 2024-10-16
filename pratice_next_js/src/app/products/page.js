704995bac97e33a3ee0134b8ad5b13d5512094c8
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-16 20:20:49 +0500

    create products page

---
+++ pratice_next_js/src/app/products/page.js
@@ -0,0 +1,10 @@
+export default function ProductList() {
+    return (
+        <>
+        <h1 className="text-3xl m-2">Product List</h1>
+        <h2 className="text-2xl m-1">Product 1</h2>
+        <h2 className="text-2xl m-1">Product 2</h2>
+        <h2 className="text-2xl m-1">Product 3</h2>
+        </>
+    )
+}
