6f0e089fe5be44bcea9533cb762f93c3fa2834f1
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-16 20:09:37 +0500

    create first page blog

---
+++ pratice_next_js/src/app/blog/first/page.js
@@ -0,0 +1,8 @@
+function FirstBlog() {
+    return (
+        <h1 className="text-3xl text-gray-500 font-bold underline text-center mt-3">
+            First blog post
+        </h1>
+    )
+}
+export default FirstBlog;
---
+++ pratice_next_js/src/app/blog/second/page.js
@@ -0,0 +1,8 @@
+function SecondBlog() {
+    return (
+        <h1 className="text-3xl text-gray-500 font-bold underline text-center mt-3">
+            Second blog post
+        </h1>
+    )
+}
+export default SecondBlog;
